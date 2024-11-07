82971b80dc6bdc7303425db72591739c41396c04
Jenni4B <jennifer.ea.blocker@gmail.com>
2024-11-07 14:53:23 -0500

    changed the log in loadArray func

--- JS Files/gap.js
+++ JS Files/gap.js
@@ -26,6 +26,14 @@ const jobData = [
  * Write a function that finds the experience level with 
  * the highest gap and returns its name and gap value
  * 
+ * Using the data from Mon. Wed. & Thurs, create the expected output
+ * 
+ * Cybersecurity Talent-Gap Analysis:
+ * 
+ * Total Talent Gap: 300,000
+ * 
+ * Highest Shortage: Mid-level with a gap of 200,000
+ * 
  */
 
 function findLargestOpenings(data){
@@ -35,9 +43,9 @@ function findLargestOpenings(data){
 
     console.log("data => ", data)
 }
+
 function loadArray(data){
-    console.log("Running findLargestOpenings. Finding the XP lvl with the highest" + 
-        " gap and returns it's name and gap value"
+    console.log("Running loadArray. Outputs array data into the console"
     )
 
     console.log("data => ", data)
